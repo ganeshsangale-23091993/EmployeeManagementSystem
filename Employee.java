9bb237ff52740384876b5213fafb35238c2a79a6
Ganesh Sangale <ganeshsangale1006@gmail.com>
2026-06-08 13:51:24 +0530

    Updated employee salary calculation

--- Employee.java
+++ Employee.java
@@ -2,5 +2,6 @@ public class Employee {
     public static void main(String[] args) {
         System.out.println("Employee Management System");
         System.out.println("Login Module Added");
+        System.out.println("Payment Module Added");
     }
 }
