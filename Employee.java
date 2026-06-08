697fc6014cf6f35db7b1c40d0b54fdce83a93e1d
Ganesh Sangale <ganeshsangale1006@gmail.com>
2026-06-08 13:40:30 +0530

    Login Module Class Created

--- Employee.java
+++ Employee.java
@@ -1,5 +1,6 @@
 public class Employee {
     public static void main(String[] args) {
         System.out.println("Employee Management System");
+        System.out.println("Login Module Added");
     }
 }
